6344a27aa8fefffad18b635ee88962773cd1b11d
Okwudili Pat-Nebe <cp.nebe@gmail.com>
2024-08-29 13:58:49 +0100

    Fix formatting.
    Signed-off-by: Okwudili Pat-Nebe <cp.nebe@gmail.com>

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,10 +4,9 @@ use lightswitch::ksym::KsymIter;
 pub fn benchmark_kysm_readallkysms(c: &mut Criterion) {
     let mut group = c.benchmark_group("Read /proc/kallsyms");
     group.warm_up_time(std::time::Duration::from_secs(5));
-    group.bench_function(
-        "Measure read all ksyms",
-        |b: &mut criterion::Bencher| b.iter(|| KsymIter::from_kallsyms().collect::<Vec<_>>()),
-    );
+    group.bench_function("Measure read all ksyms", |b: &mut criterion::Bencher| {
++        b.iter(|| KsymIter::from_kallsyms().collect::<Vec<_>>())
++    });
     group.finish();
 }
 
