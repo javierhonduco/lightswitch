49b41338add6c4010d1ac8140d86dfc9581ff727
Okwudili Pat-Nebe <cp.nebe@gmail.com>
2024-08-29 08:42:32 +0100

    Cleanup

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,10 +2,13 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lightswitch::ksym::KsymIter;
 
 pub fn benchmark_kysm_readallkysms(c: &mut Criterion) {
-    c.bench_function(
-        "Read /proc/kallsyms",
+    let mut group = c.benchmark_group("Read /proc/kallsyms");
+    group.warm_up_time(std::time::Duration::from_secs(5));
+    group.bench_function(
+        "Measure read all ksyms",
         |b: &mut criterion::Bencher| b.iter(|| KsymIter::from_kallsyms().collect::<Vec<_>>()),
     );
+    group.finish();
 }
 
 criterion_group!(benches, benchmark_kysm_readallkysms);
