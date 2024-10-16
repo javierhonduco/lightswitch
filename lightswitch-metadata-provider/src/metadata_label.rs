75dbef35551dfc2349d7e3c20b07b4370351a651
Okwudili Pat-Nebe <cp.nebe@gmail.com>
2024-10-16 23:05:59 +0100

    .

--- lightswitch-metadata-provider/src/metadata_label.rs
+++ lightswitch-metadata-provider/src/metadata_label.rs
@@ -1,11 +1,11 @@
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum MetadataLabelValue {
     String(String),
     /// Value and unit.
     Number(i64, String),
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct MetadataLabel {
     pub key: String,
     pub value: MetadataLabelValue,
