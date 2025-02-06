92be5b7466b28282c317e618568d2194516f8e2c
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2025-02-06 14:29:11 +0000

    Delete duplicated tests
    These tests are testing the same thing multiple times
    
    Test Plan
    =========
    
    cI

--- src/cli/validators.rs
+++ src/cli/validators.rs
@@ -70,93 +70,11 @@ fn primes_before_after(non_prime: usize) -> Result<(usize, usize), String> {
 mod tests {
     use super::*;
 
-    use rand::distributions::Distribution;
-    use rand::distributions::Uniform;
-    use rstest::{fixture, rstest};
-    use std::collections::HashSet;
-
-    // Powers of 2 in usize range
-    #[fixture]
-    fn power_of_two_usize() -> Vec<usize> {
-        let mut test_usizes = vec![];
-        for shift in 0..63 {
-            let val: usize = 2 << shift;
-            test_usizes.push(val);
-        }
-        test_usizes
-    }
-
-    // Powers of 2 represented as Strings
-    #[fixture]
-    fn power_of_two_strings(power_of_two_usize: Vec<usize>) -> Vec<String> {
-        let mut test_uint_strings = vec![];
-        for val in power_of_two_usize {
-            let val_str = val.to_string();
-            test_uint_strings.push(val_str);
-        }
-        test_uint_strings
-    }
-
-    // This fixture produces 5 million random results from the range of usize
-    // integers that are NOT powers of 2
-    #[fixture]
-    fn all_but_power_of_two_usize(power_of_two_usize: Vec<usize>) -> Vec<usize> {
-        let mut test_usize_set: HashSet<usize> = HashSet::new();
-        let mut test_usize_not_p2: Vec<usize> = vec![];
-        // usizes that ARE powers of two, for later exclusion
-        for val in power_of_two_usize {
-            test_usize_set.insert(val);
-        }
-        // Now, for a random sampling of 500000 integers in the range of usize,
-        // excluding any that are known to be powers of 2
-        let between = Uniform::from(0..=usize::MAX);
-        let mut rng = rand::thread_rng();
-        for _ in 0..500000 {
-            let usize_int: usize = between.sample(&mut rng);
-            if test_usize_set.contains(&usize_int) {
-                // We know this is a power of 2, already tested separately, skip
-                continue;
-            }
-            test_usize_not_p2.push(usize_int);
-        }
-        test_usize_not_p2
-    }
-
-    // all_but_power_of_two_usize, but as Strings
-    #[fixture]
-    fn all_but_power_of_two_strings(all_but_power_of_two_usize: Vec<usize>) -> Vec<String> {
-        let mut test_uint_strings: Vec<String> = vec![];
-        for val in all_but_power_of_two_usize {
-            let val_str = val.to_string();
-            test_uint_strings.push(val_str);
-        }
-        test_uint_strings
-    }
-
-    #[rstest]
-    fn args_should_be_powers_of_two(power_of_two_strings: Vec<String>) {
-        for val_string in power_of_two_strings {
-            assert!(value_is_power_of_two(val_string.as_str()).is_ok())
-        }
-    }
-
-    #[rstest]
-    fn args_should_not_be_powers_of_two(all_but_power_of_two_strings: Vec<String>) {
-        for non_p2_string in all_but_power_of_two_strings {
-            let result = value_is_power_of_two(non_p2_string.as_str());
-            assert!(result.is_err());
-        }
-    }
+    use rstest::rstest;
 
     #[rstest]
     #[case(49, (47,53), "")]
     #[case(97, (0, 0), "97 is prime")]
-    #[case(100, (97,101), "")]
-    #[case(398, (397,401), "")]
-    #[case(500, (499,503), "")]
-    #[case(1000, (997, 1009), "")]
-    #[case(1001, (997, 1009), "")]
-    #[case(1009, (0, 0), "1009 is prime")]
     fn test_primes_before_after(
         #[case] non_prime: usize,
         #[case] expected_tuple: (usize, usize),
