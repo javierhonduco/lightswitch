efc069893c75f6a3514fa645656a724b3b9408f8
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2025-07-18 10:09:19 +0100

    object: Use 2024 edition
    The current code won't compile with `cargo 1.90.0-nightly (6833aa715 2025-07-13)`
    
    ```
       Compiling lightswitch-object v0.3.1 (/Users/javierhonduco/src/lightswitch/lightswitch-object)
    error: let chains are only allowed in Rust 2024 or later
       --> lightswitch-object/src/object.rs:158:30
        |
    158 |                 if is_zig && let Some((low_address, high_address)) = zig_first_frame {
        |                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    ```
    
    Upgrading to 2024 edition to make this compile for older and newer versions of rustc.
    
    Test Plan
    =========
    
    CI

--- lightswitch-object/src/lib.rs
+++ lightswitch-object/src/lib.rs
@@ -1,13 +1,12 @@
-#![feature(let_chains)]
 mod buildid;
 pub mod kernel;
 mod object;
 
-pub use object::code_hash;
 pub use object::ElfLoad;
 pub use object::ObjectFile;
 pub use object::Runtime;
 pub use object::StopUnwindingFrames;
+pub use object::code_hash;
 
 pub use buildid::BuildId;
 pub use buildid::ExecutableId;
