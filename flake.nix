0b5140563e79b80c8a436b80aa5a5bd99e4d6ebe
Gordon Marler <gmarler@bloomberg.net>
2025-10-07 10:32:06 -0400

    Add clang-format to prevent breaking eBPF format changes
    - Add a justfile and cmd to reformat BPF files from cli
    - Add just to the Nix flake

--- flake.nix
+++ flake.nix
@@ -11,95 +11,111 @@
 
     crane.url = "github:ipetkov/crane";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          elfutils' = (pkgs.elfutils.override { enableDebuginfod = false; }).overrideAttrs (attrs: {
-            doCheck = false;
-            doInstallCheck = false;
-            configureFlags = attrs.configureFlags ++ [ "--without-zstd" ];
-            nativeBuildInputs = attrs.nativeBuildInputs ++ [ pkgs.pkg-config ];
-          });
-          buildInputs = with pkgs; [
-            llvmPackages_16.clang
-            llvmPackages_16.libcxx
-            llvmPackages_16.libclang
-            llvmPackages_16.lld
-            elfutils'
-            zlib.static
-            zlib.dev
-            glibc
-            glibc.static
-            protobuf
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          rust-toolchain = pkgs.rust-bin.nightly.latest.default;
-          craneLib = (crane.mkLib nixpkgs.legacyPackages.${system}).overrideToolchain rust-toolchain;
-          lightswitch = craneLib.buildPackage {
-            src = ./.;
-            doCheck = false;
-            buildInputs = buildInputs;
-            nativeBuildInputs = nativeBuildInputs;
-            hardeningDisable = [ "all" ];
-            LIBCLANG_PATH = with pkgs; lib.makeLibraryPath [ llvmPackages_16.libclang ];
-            LIBBPF_SYS_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ zlib.static elfutils' ];
-          };
-        in
-        with pkgs;
-        {
-          formatter = pkgs.nixpkgs-fmt;
-          packages = {
-            default = lightswitch;
-            container = pkgs.dockerTools.buildLayeredImage {
-              name = "lightswitch";
-              config = {
-                Entrypoint = [ "${lightswitch}/bin/lightswitch" ];
-                Env = [
-                  "RUST_BACKTRACE=1"
-                ];
-              };
-            };
-            vmtest = (import ./vm.nix { inherit pkgs; }).run-vmtest lightswitch;
-          };
-          devShells.default = mkShell {
-            nativeBuildInputs = nativeBuildInputs;
-            buildInputs = buildInputs ++ [
-              (rust-toolchain.override {
-                extensions = [
-                  "rust-src"
-                  "rust-analyzer"
-                ];
-              })
-              # Debugging tools
-              strace
-              gdb
-              bpftools
-              # Upload container image to registry
-              skopeo
-              # Cargo subcommand tools
-              ## To upgrade deps
-              cargo-edit
-              ## Snapshot testing
-              cargo-insta
-              ## Remove unused deps
-              cargo-shear
-              ## Release to crates.io
-              cargo-release
-              # Commented out because this is typically not cached and it's rarely used
-              # ocamlPackages.magic-trace
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    , crane
+    ,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        elfutils' = (pkgs.elfutils.override { enableDebuginfod = false; }).overrideAttrs (attrs: {
+          doCheck = false;
+          doInstallCheck = false;
+          configureFlags = attrs.configureFlags ++ [ "--without-zstd" ];
+          nativeBuildInputs = attrs.nativeBuildInputs ++ [ pkgs.pkg-config ];
+        });
+        buildInputs = with pkgs; [
+          llvmPackages_16.clang
+          llvmPackages_16.libcxx
+          llvmPackages_16.libclang
+          llvmPackages_16.lld
+          elfutils'
+          zlib.static
+          zlib.dev
+          glibc
+          glibc.static
+          protobuf
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+        rust-toolchain = pkgs.rust-bin.nightly.latest.default;
+        craneLib = (crane.mkLib nixpkgs.legacyPackages.${system}).overrideToolchain rust-toolchain;
+        lightswitch = craneLib.buildPackage {
+          src = ./.;
+          doCheck = false;
+          buildInputs = buildInputs;
+          nativeBuildInputs = nativeBuildInputs;
+          hardeningDisable = [ "all" ];
+          LIBCLANG_PATH = with pkgs; lib.makeLibraryPath [ llvmPackages_16.libclang ];
+          LIBBPF_SYS_LIBRARY_PATH =
+            with pkgs;
+            lib.makeLibraryPath [
+              zlib.static
+              elfutils'
             ];
-            hardeningDisable = [ "all" ];
-            LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages_16.libclang ];
-            LIBBPF_SYS_LIBRARY_PATH = lib.makeLibraryPath [ zlib.static elfutils' ];
-            RUST_GDB = "${gdb}/bin/gdb";
+        };
+      in
+      with pkgs;
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          default = lightswitch;
+          container = pkgs.dockerTools.buildLayeredImage {
+            name = "lightswitch";
+            config = {
+              Entrypoint = [ "${lightswitch}/bin/lightswitch" ];
+              Env = [
+                "RUST_BACKTRACE=1"
+              ];
+            };
           };
-        }
-      );
+          vmtest = (import ./vm.nix { inherit pkgs; }).run-vmtest lightswitch;
+        };
+        devShells.default = mkShell {
+          nativeBuildInputs = nativeBuildInputs;
+          buildInputs = buildInputs ++ [
+            (rust-toolchain.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+              ];
+            })
+            # Debugging tools
+            strace
+            gdb
+            bpftools
+            # Upload container image to registry
+            skopeo
+            # Cargo subcommand tools
+            ## To upgrade deps
+            cargo-edit
+            ## Snapshot testing
+            cargo-insta
+            ## Remove unused deps
+            cargo-shear
+            ## Release to crates.io
+            cargo-release
+            # Commented out because this is typically not cached and it's rarely used
+            # ocamlPackages.magic-trace
+            just
+          ];
+          hardeningDisable = [ "all" ];
+          LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages_16.libclang ];
+          LIBBPF_SYS_LIBRARY_PATH = lib.makeLibraryPath [
+            zlib.static
+            elfutils'
+          ];
+          RUST_GDB = "${gdb}/bin/gdb";
+        };
+      }
+    );
 }
